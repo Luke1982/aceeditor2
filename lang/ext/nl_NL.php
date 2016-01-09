d38572efef41ac3122e7fb855cb482112006898d
Guido Goluke <gcx3@hotmail.com>
2016-01-09 15:26:01 +0100

    Adds dutch translation

---
+++ lang/ext/nl_NL.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *
+ * Copyright:
+ *
+ * MajorLabel - Guido Goluke
+ * Web: www.majorlabel.nl
+ * Email: info@majorlabel.nl
+ *
+ *
+ * Authors:
+ *
+ * Guido Goluke, <info@majorlabel.nl>
+ *  
+ *
+ * License:
+ *-------------------------------------------------------------------------
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
+ *
+ * ------------------------------------------------------------------------- */
+$lang['friendlyname'] 			= 'Ace Editor voor CMSMS 2.x';
+$lang['admindescription'] 		= 'Implementeert de Ace Editor als syntax highlighter voor CMSMS 2.x';
+$lang['ask_unistall'] 			= 'Weet u zeker dat u Ace Editor 2.0 volledig wilt verwijderen?';
+
+// Preferences page
+$lang['prefspage_title'] 		= 'Ace Editor 2 Voorkeuren';
+$lang['prefspage_intro'] 		= 'U kunt hier wat voorkeuren instellen voor de Ace Editor. Gebruik het type breedte om aan te geven of de editor een percentage of een breedte in pixels moet aannnemen. Gebruik vervolgens de slider (als u een voorkeur voor percentage heeft ingesteld), of het invoerveld (als u een voorkeur voor pixels heeft ingesteld) om de startbreedte van de editor in the vullen. <b>Let op</b>, De rechter zijkant en de onderkant van de editor kunnen altijd versleept worden om de editor tijdens gebruik in de breedte of de hoogte te verstellen (wordt niet opgeslagen als voorkeur). In het laatste invoerveld kunt u de standaard hoogte in pixels instellen.';
+$lang['width_pref_label'] 		= 'Stel voorkeur voor type breedte in';
+$lang['width_pref_percent'] 	= 'Percentage';
+$lang['width_pref_pixels'] 		= 'Pixels';
+$lang['width_perc_label'] 		= 'Beweeg de slider naar het gewenste percentage (tussen 5% en 95%)';
+$lang['width_pixels_label'] 	= 'Voer de gewenste breedte in pixels in';
+$lang['height_pixels_label'] 	= 'Voer de gewenste hoogte in pixels in';
+$lang['save_prefs'] 			= 'Sla Ace voorkeuren op';
+$lang['pref_saved_mess']		= 'Ace voorkeuren opgeslagen';
+$lang['css_prefmode_label']		= 'Kies de voorkeursmode tijdens het bewerken van CSS';
+$lang['cssmode_css']			= 'CSS';
+$lang['cssmode_less']			= 'LESS';
+$lang['cssmode_sass']			= 'SASS';
